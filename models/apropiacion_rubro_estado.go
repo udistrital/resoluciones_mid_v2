25b6f742f09361e3e66746abb0b25a5333512b2d
jupmorenor <jupmorenor@gmail.com>
2022-04-20 22:04:28 -0500

    fix: Se eliminan archivos que no se usan. udistrital/resoluciones_cliente_v2#36

--- models/apropiacion_rubro_estado.go
+++
@@ -1,8 +0,0 @@
-package models
-
-//ApropiacionRubroEstado modelo para el estado de la apropiación en financiera
-type ApropiacionRubroEstado struct {
-	Id          int
-	Nombre      string
-	Descripcion string
-}
