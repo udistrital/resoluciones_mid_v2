02c498e1b90fe0b1c49fd4a8a2a22cb0eac3a65c
jupmorenor <jupmorenor@gmail.com>
2022-04-20 22:10:28 -0500

    fix: se elimina codigo no utilizado. udistrital/resoluciones_cliente_v2#36

--- helpers/expedir_resolucion.go
+++ helpers/expedir_resolucion.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/astaxie/beego/logs"
 	"github.com/udistrital/resoluciones_mid_v2/models"
 )
 
@@ -53,64 +52,4 @@ func CalcularFechaFin(fecha_inicio time.Time, numero_semanas int) (fecha_fin tim
 	f_i := fecha_inicio
 	after := f_i.AddDate(0, entero, int(numero_dias))
 	return after
-}
-
-func GetContenidoResolucion(id_resolucion string, id_facultad string) (contenidoResolucion models.ResolucionCompleta, outputError map[string]interface{}) {
-
-	defer func() {
-		if err := recover(); err != nil {
-			outputError = map[string]interface{}{"funcion": "/GetContenidoResolucion", "err": err, "status": "502"}
-			panic(outputError)
-		}
-	}()
-
-	var ordenador_gasto []models.OrdenadorGasto
-	var jefe_dependencia []models.JefeDependencia
-
-	url := "contenido_resolucion/" + id_resolucion
-	if err1 := GetRequestNew("UrlCrudResoluciones", url, &contenidoResolucion); err1 == nil {
-		url = "ordenador_gasto?limit=-1&query=DependenciaId:" + id_facultad
-		if err2 := GetRequestLegacy("UrlcrudCore", url, &ordenador_gasto); err2 == nil {
-			fmt.Println(ordenador_gasto)
-			if ordenador_gasto == nil || len(ordenador_gasto) == 0 {
-				url = "ordenador_gasto?query=Id:1"
-				if err3 := GetRequestLegacy("UrlcrudCore", url, &ordenador_gasto); err3 == nil {
-					contenidoResolucion.OrdenadorGasto = ordenador_gasto[0]
-				} else {
-					logs.Error(err3)
-					outputError = map[string]interface{}{"funcion": "/GetContenidoResolucion3", "err3": err3, "status": "502"}
-					return contenidoResolucion, outputError
-				}
-			} else {
-				contenidoResolucion.OrdenadorGasto = ordenador_gasto[0]
-			}
-
-		} else {
-			logs.Error(err2)
-			outputError = map[string]interface{}{"funcion": "/GetContenidoResolucion2", "err2": err2, "status": "502"}
-			return contenidoResolucion, outputError
-		}
-	} else {
-		logs.Error(err1)
-		outputError = map[string]interface{}{"funcion": "/GetContenidoResolucion1", "err": err1, "status": "502"}
-		return contenidoResolucion, outputError
-	}
-
-	fecha_actual := time.Now().Format("2006-01-02")
-	var err5 map[string]interface{}
-	url = "jefe_dependencia?query=DependenciaId:" + id_facultad + ",FechaFin__gte:" + fecha_actual + ",FechaInicio__lte:" + fecha_actual
-	if err4 := GetRequestLegacy("UrlcrudCore", url, &jefe_dependencia); err4 == nil {
-		contenidoResolucion.OrdenadorGasto.NombreOrdenador, err5 = BuscarNombreProveedor(jefe_dependencia[0].TerceroId)
-		if err5 != nil {
-			logs.Error(err4)
-			//outputError = map[string]interface{}{"funcion": "/GetContenidoResolucion5", "err5": err5, "status": "502"}
-			return contenidoResolucion, err5
-		}
-	} else {
-		logs.Error(err4)
-		outputError = map[string]interface{}{"funcion": "/GetContenidoResolucion4", "err4": err4, "status": "502"}
-		return contenidoResolucion, outputError
-	}
-
-	return contenidoResolucion, nil
 }
