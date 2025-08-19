982f1fb01fad658baae11af9660fdc5349eee8ec
aalejoz25 <alvaroalejo25@gmail.com>
2025-08-18 19:52:36 -0500

    hotfix: actualizacion de resoluciones_mid_v2 udistrital/resoluciones_cliente_v2#229
    Se redefine modelo de contrato para hacer uso de api de parametros

--- models/contrato_preliquidacion.go
+++ models/contrato_preliquidacion.go
@@ -3,26 +3,22 @@ package models
 import "time"
 
 type ContratoPreliquidacion struct {
-	NumeroContrato            string
-	Vigencia                  int
-	NombreCompleto            string
-	Documento                 string
-	PersonaId                 int
-	TipoNominaId              int
-	FechaInicio               time.Time
-	FechaFin                  time.Time
-	ValorContrato             float64
-	DependenciaId             int
-	Cdp                       int
-	Rp                        int
-	Activo                    bool
-	NumeroSemanas             int
-	ResolucionId              int
-	Resolucion                string
-	PorcentajeCesantias       float64
-	PorcentajePrimaServicios  float64
-	PorcentajePrimaNavidad    float64
-	PorcentajeVacaciones      float64
-	PorcentajePrimaVacaciones float64
-	Desagregado               *map[string]float64
+	NumeroContrato           string
+	Vigencia                 int
+	NombreCompleto           string
+	Documento                string
+	PersonaId                int
+	TipoNominaId             int
+	FechaInicio              time.Time
+	FechaFin                 time.Time
+	ValorContrato            float64
+	DependenciaId            int
+	Cdp                      int
+	Rp                       int
+	Activo                   bool
+	NumeroSemanas            int
+	ResolucionId             int
+	Resolucion               string
+	PorcentajesDesagregadoId int
+	Desagregado              *map[string]float64
 }
