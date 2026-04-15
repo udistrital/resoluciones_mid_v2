77ed08c8bd78ec58dd777c02a680b506feb20dfd
cristhianLagunaM <claguna201@gmail.com>
2026-04-15 12:11:57 -0500

    fix: ajuste del consumo de ids segun el entorno

--- models/datos_reporte.go
+++ models/datos_reporte.go
@@ -1,8 +1,13 @@
 package models
 
 type DatosReporte struct {
-	Resolucion     string
-	NivelAcademico string
-	Facultad       int
-	Vigencia       int
+	Resolucion                  string
+	NivelAcademico              string
+	Facultad                    int
+	Vigencia                    int
+	TipoResolucionVinculacionId int
+	TipoResolucionAdicionId     int
+	TipoResolucionReduccionId   int
+	TipoResolucionCancelacionId int
+	EstadoResolucionExpedidaId  int
 }
